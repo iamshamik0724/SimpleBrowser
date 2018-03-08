5cc8bd4986b2a1883e33a04d52f3a381920644d3
Shamik Khan <iamshamik0724@gmail.com>
2018-03-09 04:24:35 +0530

    Simple Browser

---
+++ browser_main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+public class browser_main {
+
+	public static void main(String[] args){
+		
+		Browser_Frame go = new Browser_Frame();
+		go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		go.setSize(300,200);
+		go.setVisible(true);
+		
+		
+	}
+}
